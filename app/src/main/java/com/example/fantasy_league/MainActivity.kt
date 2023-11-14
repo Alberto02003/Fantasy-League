bf122a6fb0b7dfb016a4786b2aecf0408f3d2463
alberto <alberto03009@gmail.com>
2023-11-14 18:46:25 +0000

    0.1

--- app/src/main/java/com/example/fantasy_league/MainActivity.kt
+++ app/src/main/java/com/example/fantasy_league/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.fantasy_league
 
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.example.fantasy_league.ui.theme.FantasyLeagueTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +28,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FantasyLeagueTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Column {
+                    Text(text = "Bienvenido a tu Fantasy")
+                    Row (){
+                        Button(onClick = {
+
+                        }) {
+                            Text(text = "Add")
+                        }
+                        Button(onClick = {
+
+                        }) {
+                            Text(text = "Delete")
+                        }
+                    }
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FantasyLeagueTheme {
-        Greeting("Android")
-    }
-}
+
+
+
