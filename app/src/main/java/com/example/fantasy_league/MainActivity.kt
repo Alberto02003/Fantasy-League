736c1f4453576ffe8370517f62a247c453e2d14b
alberto <alberto03009@gmail.com>
2023-11-22 19:42:22 +0000

    0.2

--- app/src/main/java/com/example/fantasy_league/MainActivity.kt
+++ app/src/main/java/com/example/fantasy_league/MainActivity.kt
@@ -4,14 +4,22 @@ import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,8 +27,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.fantasy_league.ui.theme.FantasyLeagueTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,22 +41,63 @@ class MainActivity : ComponentActivity() {
         setContent {
             FantasyLeagueTheme {
                 Column {
-                    Text(text = "Bienvenido a tu Fantasy")
-                    Row (){
-                        Button(onClick = {
-
-                        }) {
-                            Text(text = "Add")
-                        }
-                        Button(onClick = {
-
-                        }) {
-                            Text(text = "Delete")
+                    Row {
+                        Text(text = "Bienvenido a tu Fantasy")
+                    }
+                    Card (colors = CardDefaults.cardColors(
+                        containerColor = Color.Gray,
+                    ), modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)) {
+                        Row  {
+                            Column {
+                                Image(painter = painterResource(id = R.drawable.cris) , contentDescription ="" , modifier = Modifier.size(150.dp))
+                            }
+                            Column (verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()){
+                                Text(text = "Cristiano Ronaldo")
+                                Text(text = "vs Getafe 2017")
+                                Text(text = "4 goles")
+                            }
                         }
                     }
-                }
+                    Card (colors = CardDefaults.cardColors(
+                        containerColor = Color.Red,
+                    ), modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)) {
+                        Row  {
+                            Column {
+                                Image(painter = painterResource(id = R.drawable.messi) , contentDescription ="" , modifier = Modifier.size(150.dp))
+                            }
+                            Column (verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()){
+                                Text(text = "Messi")
+                                Text(text = "vs Real Madrid")
+                                Text(text = "4 goles en propia")
+                            }
+                        }
+                        Row (){
+                            Column {
+                                Button(onClick = {
 
-            }
+                                }) {
+                                    Text(text = "Add")
+                                }
+                            }
+                            Column {
+                                Button(onClick = {
+
+                                }) {
+                                    Text(text = "Delete")
+                                }
+                            }
+
+                        }
+                    }
+
+
+
+                    }
+                }
         }
     }
 }
