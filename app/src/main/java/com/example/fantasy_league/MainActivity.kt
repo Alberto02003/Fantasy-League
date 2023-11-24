47e99eb12792717cb57063647f077305c904f1d0
alberto <alberto03009@gmail.com>
2023-11-24 17:59:35 +0000

    0.4

--- app/src/main/java/com/example/fantasy_league/MainActivity.kt
+++ app/src/main/java/com/example/fantasy_league/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -18,12 +19,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
@@ -42,10 +47,15 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.fantasy_league.Metodos.obtenerEquiposUnicos
 import com.example.fantasy_league.Objetos.jugador
+import com.example.fantasy_league.Objetos.jugadores
 import com.example.fantasy_league.ui.theme.FantasyLeagueTheme
+import java.util.function.Consumer
 
 class MainActivity : ComponentActivity() {
+
+    val equipois = obtenerEquiposUnicos(jugadores.listajugador)
 
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,48 +63,70 @@ class MainActivity : ComponentActivity() {
         setContent {
             val ctx = LocalContext.current
             FantasyLeagueTheme {
-                Column {
+                Column (modifier = Modifier.fillMaxSize()) {
                     Text(text = "Bienvenido a tu Fantasy")
-                    var query by remember { mutableStateOf("") }
-                    var active by remember { mutableStateOf(false) }
-                    SearchBar(
-                        query = query,
-                        onQueryChange = {query = it},
-                        onSearch = {
-                            Toast.makeText(ctx, query, Toast.LENGTH_SHORT).show()
-                            active = false
-                        },
-                        active = active,
-                        onActiveChange ={active = it},
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        //LazyColumn(content = )
-                    }
-                    Row {
-                        Column (verticalArrangement = Arrangement.Bottom){
-                            Button(onClick = {
-
-                            }) {
-                                Text(text = "Add")
-                                Icon(painter = painterResource(id = R.drawable.anadir), modifier = Modifier
-                                    .size(30.dp)
-                                    .padding(horizontal = 5.dp), contentDescription ="" )
-                            }
-                        }
-                            Button(onClick = {
-                            }) {
-                                Text(text = "Delete")
-                                Icon(painter = painterResource(id = R.drawable.eliminar), modifier = Modifier
-                                    .size(30.dp)
-                                    .padding(horizontal = 5.dp), contentDescription ="" )
-                            }
-                        }
+                    val ctx = LocalContext.current
+                    var listaFiltrada: ArrayList<jugador>
+                    SeachBar(ctx)
+                    Botones()
                     }
                 }
         }
     }
 }
 
+
+@Composable
+fun Botones(){
+    Row (verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.SpaceAround, modifier = Modifier.fillMaxSize()) {
+        ExtendedFloatingActionButton(onClick = {
+
+        }) {
+            Text(text = "Add")
+            Icon(painter = painterResource(id = R.drawable.anadir), modifier = Modifier
+                .size(30.dp)
+                .padding(horizontal = 5.dp), contentDescription ="" )
+        }
+        ExtendedFloatingActionButton(onClick = {
+        }) {
+            Text(text = "Delete")
+            Icon(painter = painterResource(id = R.drawable.eliminar), modifier = Modifier
+                .size(30.dp)
+                .padding(horizontal = 5.dp), contentDescription ="" )
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SeachBar(Ctx:ctx){
+    var query by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+    SearchBar(
+        query = query,
+        onQueryChange = {query = it},
+        onSearch = {
+            Toast.makeText(ctx, query, Toast.LENGTH_SHORT).show()
+            active = false
+        },
+        active = active,
+        onActiveChange ={active = it},
+        modifier = Modifier.fillMaxWidth(),
+        trailingIcon = {
+            IconButton(onClick = { active = false}) {
+                Icon(painter = painterResource(id = R.drawable.buscar), contentDescription = "", modifier = Modifier.size(30.dp))
+            }
+        }
+    ) {
+        LazyColumn(){
+            listaFiltrada =
+                jugadores.listajugador.filter { it.equipo.startsWith(query, true) } as ArrayList<jugador>
+            items(listaFiltrada){
+                androidx.compose.material3.ListItem(modifier = Modifier.clickable { query = it.equipo },headlineContent = { Text(it.equipo)})
+            }
+        }
+
+    }
+}
 @Composable
 fun carta(jugador:jugador){
     Card (colors = CardDefaults.cardColors(
--- app/src/main/java/com/example/fantasy_league/Metodos/Metodos.kt
+++ app/src/main/java/com/example/fantasy_league/Metodos/Metodos.kt
@@ -6,9 +6,22 @@ fun valorInicial(): ArrayList<jugador>{
     var listajugador =ArrayList<jugador>()
     var jugador = jugador("Cristriano Ronaldo","REAL MADRID", "EI", 5, "Barceponga", "29/10/2017")
     var jugador2 = jugador("Messi","Barcelona", "ED", 0, "Real Madrid", "29/10/2017")
+    var jugador3 = jugador("Joe Hart","Celtic Glashgow", "POR", 0 , "Real Madrid", "29/10/2017")
+
 
     listajugador.add(jugador)
     listajugador.add(jugador2)
+    listajugador.add(jugador3)
 
     return listajugador
+}
+
+fun obtenerEquiposUnicos(listaJugadores: List<jugador>): List<String> {
+    val equiposUnicos = mutableSetOf<String>()
+
+    for (jugador in listaJugadores) {
+        equiposUnicos.add(jugador.equipo)
+    }
+
+    return equiposUnicos.toList()
 }
